d62482cd955430d8721044cc8717f3773300c0c9
o0D14 <kerrmare@gmail.com>
2026-06-29 21:27:49 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+const myButton = document.getElementById('click');
+
+myButton.addEventListener('click', () => {
+  window.location.href = 'click.html'; 
+});
+
+/*переход с главного на тап*/
+
+
---
+++ save-all.js
@@ -0,0 +1,19 @@
+// 1. Браузер заходит в память и ищет сохранённый баланс. 
+// Если игрок зашел впервые и там пусто, ставим 0.
+let score = Number(localStorage.getItem('playerScore')) || 0;
+
+// 2. Находим кнопку "Tap" и текст "balance" на странице
+const balanceText = document.getElementById('balance');
+const clickButton = document.getElementById('Tap');
+
+// 3. Сразу же показываем игроку его сохранённый баланс, как только он открыл сайт
+balanceText.textContent = 'Баланс: ' + score;
+
+// 4. Слушаем клики по кнопке
+clickButton.addEventListener('click', () => {
+    score = score + 1;                            // Прибавляем единицу
+    balanceText.textContent = 'Баланс: ' + score; // Меняем цифру на экране
+    
+    // 5. САМО СОХРАНЕНИЕ: записываем новое число в память браузера
+    localStorage.setItem('playerScore', score);
+});
