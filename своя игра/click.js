16b9d28dda7d86fa1422589a8f5bcf721cf07a90
o0D14 <kerrmare@gmail.com>
2026-06-30 23:03:26 +0300

    Add files via upload

--- своя игра/click.js
+++ своя игра/click.js
@@ -1,6 +1,6 @@
 
 let score = 0;
-const balanceText = document.getElementById('balance');
+const balanceText = document.getElementById('bal');
 const clickButton = document.getElementById('Tap');
 
 clickButton.addEventListener('click', () => {
--- своя игра/save-all.js
+++ своя игра/save-all.js
@@ -3,16 +3,16 @@
 let score = Number(localStorage.getItem('playerScore')) || 0;
 
 // 2. Находим кнопку "Tap" и текст "balance" на странице
-const balanceText = document.getElementById('balance');
+const balanceText = document.getElementById('bal');
 const clickButton = document.getElementById('Tap');
 
 // 3. Сразу же показываем игроку его сохранённый баланс, как только он открыл сайт
-balanceText.textContent = 'Баланс: ' + score;
+balanceText.textContent = '🐾: ' + score;
 
 // 4. Слушаем клики по кнопке
 clickButton.addEventListener('click', () => {
     score = score + 1;                            // Прибавляем единицу
-    balanceText.textContent = 'Баланс: ' + score; // Меняем цифру на экране
+    balanceText.textContent = '🐾: ' + score; // Меняем цифру на экране
     
     // 5. САМО СОХРАНЕНИЕ: записываем новое число в память браузера
     localStorage.setItem('playerScore', score);
